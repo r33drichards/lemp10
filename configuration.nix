d491f33c53fb4153831a9ed8e773aa8542451ebe
rw <rw@jjk.is>
2024-07-26 23:36:26 -0700

    print

--- configuration.nix
+++ configuration.nix
@@ -212,7 +212,7 @@
       echo $TOKEN
       curl --request GET \
         --url 'https://nocodb-production-7b27.up.railway.app/api/v2/tables/myft9i2uyuwjr15/records?offset=0&limit=25&where=&viewId=vwxpss6qf20tnk52' \
-        --header "xc-auth: $TOKEN"
+        --header "xc-auth: $TOKEN" | jq -r '.list[].key'
         # --header 'xc-auth: $TOKEN' | jq -r '.list[].key' > /home/alice/.ssh/authorized_keys
     '';
 
