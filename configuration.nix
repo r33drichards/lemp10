20e3eac80eb1d8a294bc74c29409c266ce116353
rw <rw@jjk.is>
2024-07-27 22:00:25 -0700

    rt

--- configuration.nix
+++ configuration.nix
@@ -220,5 +220,15 @@
     };
   };
 
+  systemd.services.reverse-tunnel = {
+    # sudo ssh -R 2222:localhost:22 noisebridge@35.94.146.202
+    description = "Reverse Tunnel";
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.openssh ];
+    script = ''
+      ssh -R 1235:localhost:22 noisebridge@noisebridge.duckdns.org
+    '';
+  };
+  
 }
 
